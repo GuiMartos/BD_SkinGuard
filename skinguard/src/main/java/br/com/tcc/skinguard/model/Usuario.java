0e5da6f11cb404e17df2835c8c4394ddc9eaa8df
GuiMartos <guilhermemartos2005@gmail.com>
2023-03-24 12:23:39 -0300

    Update Usuario.java

--- skinguard/src/main/java/br/com/tcc/skinguard/model/Usuario.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/model/Usuario.java
@@ -1,4 +1,52 @@
 package br.com.tcc.skinguard.model;
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String login;
+    private String senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

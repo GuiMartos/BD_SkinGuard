af9597eb289394883082a2b2a95129d2c06a74d4
GuiMartos <guilhermemartos2005@gmail.com>
2023-03-24 11:54:13 -0300

    API

---
+++ skinguard/src/main/java/br/com/tcc/skinguard/SkinguardApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcc.skinguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinguardApplication.class, args);
+	}
+
+}

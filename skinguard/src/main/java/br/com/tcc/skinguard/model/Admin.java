ddc885f0288c420381327006bc79b4105c857226
GuiMartos <guilhermemartos2005@gmail.com>
2023-03-24 12:25:06 -0300

    Update Admin.java

--- skinguard/src/main/java/br/com/tcc/skinguard/model/Admin.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/model/Admin.java
@@ -1,4 +1,53 @@
 package br.com.tcc.skinguard.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "admin")
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String login;
+    private String senha;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return id.equals(admin.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }

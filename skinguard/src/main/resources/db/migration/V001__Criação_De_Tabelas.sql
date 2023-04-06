8b8b3a6f16d9e294fdb7f6a5b89d9c7f26b1fd43
GuiMartos <guilhermemartos2005@gmail.com>
2023-04-06 12:25:41 -0300

    Update V001__Criação_De_Tabelas.sql

--- skinguard/src/main/resources/db/migration/V001__Criação_De_Tabelas.sql
+++ skinguard/src/main/resources/db/migration/V001__Criação_De_Tabelas.sql
@@ -1,3 +1,4 @@
+-- Tabela do FPS do filtro solar
 create table fps(
 
  id int not null primary key auto_increment,
@@ -13,13 +14,38 @@ insert into fps values(80);
 insert into fps values(90);
 insert into fps values(99);
 
+-- Tabela do dispositivo medidor
 create table eps(
   id int not null primary key auto_increment,
   esp int not null
 );
 
+
+-- Tabela do tipo de pele
 create table pele(
       id int not null primary key auto_increment,
       tom int not null
 );
+insert into pele values("Tipo 1");
+insert into pele values("Tipo 2");
+insert into pele values("Tipo 3");
+insert into pele values("Tipo 4");
+insert into pele values("Tipo 5");
+insert into pele values("Tipo 6");
+insert into pele values("Tipo 7");
+
+-- Tabela do Usuario
+create table usuario(
+    id int not null primary key auto_increment,
+    login varchar (50),
+    senha varchar(20),
+    idfps int not null,
+    idpele int not null,
+    idesp int
+
+);
+
+Alter table usuario add constraint FK_usuario_fps FOREIGN key usuario(idfps) references fps(id);
+Alter table usuario add constraint FK_usuario_pele FOREIGN key usuario(idpele) references pele(id);
+Alter table usuario add constraint Fk_usuario_esp FOREIGN key usuario(idesp) references esp(id);
 

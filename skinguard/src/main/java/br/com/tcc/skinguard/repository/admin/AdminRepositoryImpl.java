c1a983a760f34f9d9d47150c854d1121fb145a87
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-03 11:31:46 -0300

    Update AdminRepositoryImpl.java

--- skinguard/src/main/java/br/com/tcc/skinguard/repository/admin/AdminRepositoryImpl.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/repository/admin/AdminRepositoryImpl.java
@@ -5,11 +5,9 @@ import br.com.tcc.skinguard.repository.filter.AdminFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Predicates;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;

662350881fe2faaac24ef151e0b9bb1b29e9c717
GuiMartos <guilhermemartos2005@gmail.com>
2023-09-18 11:51:57 -0300

    Usuário pronto

--- skinguard/src/main/java/br/com/tcc/skinguard/repository/usuario/UsuarioRepositoryImpl.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/repository/usuario/UsuarioRepositoryImpl.java
@@ -1,4 +1,67 @@
 package br.com.tcc.skinguard.repository.usuario;
 
-public class UsuarioRepositoryImpl {
+import br.com.tcc.skinguard.model.Usuario;
+import br.com.tcc.skinguard.repository.filter.UsuarioFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioRepositoryImpl implements UsuarioRepositoryQuery{
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Page<Usuario> filtrar(UsuarioFilter usuarioFilter, Pageable pageable) {
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Usuario> criteria = builder.createQuery(Usuario.class);
+        Root<Usuario> root = criteria.from(Usuario.class);
+
+        Predicate[] predicates = criarRestricoes (usuarioFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("usuario")));
+        TypedQuery<Usuario> query = manager.createQuery(criteria);
+        adicionarRestricoesDePaginacao(query, pageable);
+
+        return new PageImpl<>(query.getResultList(),pageable, total(usuarioFilter));
+    }
+
+    private Long total(UsuarioFilter usuarioFilter){
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Usuario> root = criteria.from(Usuario.class);
+
+        Predicate[] predicates = criarRestricoes(usuarioFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("usuario")));
+
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Usuario> query, Pageable pageable){
+        int paginaAtual = pageable.getPageNumber();
+        int totalRegistros = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalRegistros;
+
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistros);
+    }
+
+    private Predicate[] criarRestricoes(UsuarioFilter usuarioFilter, CriteriaBuilder builder, Root<Usuario> root){
+        List<Predicate> predicates = new ArrayList<>();
+
+        return null;
+    }
 }
--- skinguard/src/main/java/br/com/tcc/skinguard/repository/usuario/UsuarioRepositoryQuery.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/repository/usuario/UsuarioRepositoryQuery.java
@@ -1,4 +1,10 @@
 package br.com.tcc.skinguard.repository.usuario;
 
+import br.com.tcc.skinguard.model.Usuario;
+import br.com.tcc.skinguard.repository.filter.UsuarioFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UsuarioRepositoryQuery {
+    Page<Usuario> filtrar(UsuarioFilter usuarioFilter, Pageable pageable);
 }

29d631134b5485eda29a4da7641d8b4b041a3c2c
GuiMartos <guilhermemartos2005@gmail.com>
2023-09-20 08:20:22 -0300

    Começando os Resources
    Admin ok!

--- skinguard/src/main/java/br/com/tcc/skinguard/resource/AdminResource.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/resource/AdminResource.java
@@ -1,4 +1,24 @@
 package br.com.tcc.skinguard.resource;
 
+import br.com.tcc.skinguard.model.Admin;
+import br.com.tcc.skinguard.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminResource {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @GetMapping("/todos")
+    public List<Admin> listarTodosAdmin(){
+        return adminRepository.findAll(Sort.by("admin").ascending());
+    }
 }

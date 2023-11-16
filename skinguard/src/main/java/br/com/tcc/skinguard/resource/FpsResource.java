1fefb14490f7d6b3aa0bd059c4d1645f07f3dd9e
GuiMartos <guilhermemartos2005@gmail.com>
2023-11-16 11:48:49 -0300

    Update FpsResource.java

--- skinguard/src/main/java/br/com/tcc/skinguard/resource/FpsResource.java
+++ skinguard/src/main/java/br/com/tcc/skinguard/resource/FpsResource.java
@@ -2,11 +2,14 @@ package br.com.tcc.skinguard.resource;
 
 import br.com.tcc.skinguard.model.Fps;
 import br.com.tcc.skinguard.repository.FpsRepository;
+import br.com.tcc.skinguard.repository.filter.FpsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +24,33 @@ public class FpsResource {
     public List<Fps> listarTodosFps(){
         return fpsRepository.findAll(Sort.by("fps").ascending());
     }
+
+    @GetMapping()
+    public Page<Fps> pesquisar(FpsFilter fpsFilter, Pageable pageable){
+        return fpsRepository.filtrar(fpsFilter, pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Integer id){fpsRepository.deleteById(id);}
+
+    @PutMapping("/mudar-fps/{id}")
+    public HttpStatus mudarfps(@PathVariable Integer id, @RequestBody Fps fpsRequest){
+        return fpsRepository.findById(id).map(
+                fps -> {
+                    fps.setFps(fpsRequest.getFps());
+                    fps.setUsuariofps(fpsRequest.getUsuariofps());
+                    return HttpStatus.OK;
+
+                }
+        ) .orElseGet(() -> {
+            return HttpStatus.NOT_FOUND;
+        });
+    }
+
+    @PostMapping()
+    public ResponseEntity<Fps> criar(@RequestBody Fps fps){
+        return new ResponseEntity<>(fpsRepository.save(fps),HttpStatus.CREATED);
+    }
+
 }
